c027c355da3e38909616c4530cbbb9c793a89c34
Michael Hinrichs <michaelghinrichs@gmail.com>
2013-12-27 17:19:43 -0800

    added a rotating face

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<h1>MATT IS THE PRESIDENT Neerrrrrffff<h1/>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<body>
+  <section class='container'>
+    <div class='title'>
+      
+      <h1 class='center'>
+        matt, ruler of the galaxy
+      </h1>
+      
+    </div>
+
+    <div class='center'>
+      <img class='face' src="img/matt_face_1.png">
+    </div>
+  </section>
+</body>
+</html>
